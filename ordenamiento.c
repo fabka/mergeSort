c9b9466fed06ba25086a0e04fba5854f187d0e01
lespoir <david.avila@javeriana.edu.co>
2015-09-16 19:37:29 -0500

    codigo arreglado

--- ordenamiento.c
+++ ordenamiento.c
@@ -32,6 +32,18 @@ struct Fila crearFila(char linea[]){
     return fila;
 };
 
+void crearFilaMerge(char linea[], char** retorno){
+    char *stringp = linea;
+    char *delim = " ";
+    char *token;
+    int i=0;
+    while (stringp != NULL) {
+        token = strsep(&stringp, delim);
+    	if( strcmp(token, "") != 0  ){
+    	    strcpy(retorno[i++], token);
+    	}
+    }
+};
 struct Tabla importarTabla(FILE *ptr_file){
     char buf[1000];
     struct Tabla tabla;
@@ -45,6 +57,32 @@ struct Tabla importarTabla(FILE *ptr_file){
     return tabla;
 }
 
+struct TablaMerge importarTablaMerge(char* argc[], int argv, int tamTotal)
+{
+    char buf[1000];
+    int tam = 0, i,j;
+    struct TablaMerge tabla;
+    tabla.filas = (char***)malloc(sizeof(char**)*tamTotal*600);
+    for(i = 0; i < tamTotal; i++){
+	tabla.filas[i] = (char**)malloc(sizeof(char*)*6);
+	for(j = 0; j < 6; j++){
+		tabla.filas[i][j] = (char*)malloc(sizeof(char)*100);
+    	}
+    }
+    
+    FILE* ptr_file;
+    for(i = 1; i < argv-1; i++){
+    	    ptr_file = fopen(argc[i], "r");
+	    if (ptr_file){
+		while (fgets(buf,1000, ptr_file)!=NULL){
+		    crearFilaMerge(buf,tabla.filas[tam++]);
+		}
+		fclose(ptr_file);
+	    }
+    }
+    tabla.tam = tamTotal;
+    return tabla;
+}
 struct Tabla ordenarTabla( struct Tabla tabla ){
 	int i, j, m, n;
 	for( i=1; i<tabla.tam; i++){
@@ -89,6 +127,20 @@ void exportartabla( struct Tabla tabla, char *nombreArchivo ){
     }
 }
 
+void exportartablaMerge( struct TablaMerge tabla, char *nombreArchivo ){
+    FILE *file = fopen(nombreArchivo, "w");
+    int i, j;
+    if (file != NULL){
+        for( i=0; i<tabla.tam; i++ ){
+            fprintf(file, "%8s %4s %8s %6s %8s %8s\n", tabla.filas[i][0],
+                tabla.filas[i][1], tabla.filas[i][2],
+                tabla.filas[i][3], tabla.filas[i][4],
+                tabla.filas[i][5] );
+        }
+        fclose(file);
+    }
+}
+
 void imprimirTabla( struct Tabla tabla ){
     int i, j;
     int tam = tabla.tam;
@@ -99,6 +151,14 @@ void imprimirTabla( struct Tabla tabla ){
     }
 }
 
+void imprimirTablaMerge( struct TablaMerge tabla ){
+    int i, j;
+    for(i=0; i<tabla.tam; i++){
+        for(j=0; j<6; j++ ){
+	    printf("%s ",tabla.filas[i][j]);
+        }
+    }
+}
 void ssort(struct TablaMerge arr, struct TablaMerge arrAux, int lo, int hi)
 {
     if(lo >= hi) return;
@@ -111,24 +171,34 @@ void ssort(struct TablaMerge arr, struct TablaMerge arrAux, int lo, int hi)
 void mmerge(struct TablaMerge arr, struct TablaMerge arrAux, int lo, int mid, int hi)
 {
     int k,l;
+
     for(k = lo; k <= hi; k++)
-        for(l = 0; l < 100; l++)
-        	arrAux.fila[k][l] = arr.fila[k][l];
+        for(l = 0; l < 6; l++)
+        	strcpy(arrAux.filas[k][l],arr.filas[k][l]);
     int i = lo, j = mid+1;
+
     for(k = lo; k <= hi; k++)
     {
-        if(i > mid)         
-		for(l = 0; l < 100; l++)
-			arr.fila[k][l] = arrAux.fila[j++][l];
-        else if(j > hi)     
-		for(l = 0; l < 100; l++)
-			arr.fila[k][l] = arrAux.fila[i++][l];
-        else if(arrAux.fila[i] > arrAux.fila[j]){
-		for(l = 0; l < 100; l++)
-	            arr.fila[k][l] = arrAux.fila[j++][l];
+        if(i > mid){         
+		for(l = 0; l < 6; l++){
+			strcpy(arr.filas[k][l], arrAux.filas[j][l]);
+		}
+		j++;
+        }else if(j > hi) {  
+		for(l = 0; l < 6; l++){
+			strcpy(arr.filas[k][l] , arrAux.filas[i][l]);
+		}
+		i++;
+        }else if(arrAux.filas[i] > arrAux.filas[j]){
+		for(l = 0; l < 6; l++){
+	            strcpy(arr.filas[k][l] , arrAux.filas[j][l]);
+		}
+		j++;
         }else{
-		for(l = 0; l < 100; l++)
-            		arr.fila[k][l] = arrAux.fila[i++][l];
+		for(l = 0; l < 6; l++){
+            		strcpy(arr.filas[k][l] , arrAux.filas[i][l]);
+		}
+		i++;
         }
     }
 }
@@ -136,15 +206,11 @@ void mmerge(struct TablaMerge arr, struct TablaMerge arrAux, int lo, int mid, in
 int fileTam(char *nombreArchivo){
         FILE * fp = fopen(nombreArchivo, "r");
 	int lines = 0;
-	char ch;
-        if (fp != NULL){
-		while(!feof(fp))
-		{
-		  ch = fgetc(fp);
-		  if(ch == '\n')
-		  {
-		    lines++;
-		  }
+	char buf[1000];
+    	if (fp != NULL){
+        	while (fgets(buf,1000, fp)!=NULL){
+		 lines++;		
 		}
 	}
+	return lines;
 } 
