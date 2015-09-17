91273c41d6b26b471576c9380f186ca55bd38ba6
Stiven <david.avila@javeriana.edu.co>
2015-09-17 11:47:10 -0500

    csortpexec arreglado

--- csortpexec.c
+++ csortpexec.c
@@ -10,7 +10,7 @@ int main(int argv, char* argc[]){
     int status = 1, i;
     char indice;
     pid_t childPid;
-    char* command[argv+7];
+    char** command = malloc(sizeof(char*)*(argv+10));
     char* aux;
     aux = malloc(10*sizeof(*aux));
     strcpy(aux, "sort");
@@ -24,30 +24,36 @@ int main(int argv, char* argc[]){
     aux = malloc(10*sizeof(*aux));
     strcpy(aux, "-n");
     command[3] = aux;
+    aux = malloc(20*sizeof(*aux));
+    strcpy(aux, "-k5,6");
+    command[4] = aux;
     for(i = 1; i < argv-1; i++){
 		indice = i+'0';
-		aux = malloc(5*sizeof(*aux));
+		aux = malloc(10*sizeof(*aux));
 		strcpy(aux,"tmp");
 		strcat(aux, &indice);
- 		command[i+3] = aux;
+ 		command[i+4] = aux;
 	if((childPid = fork()) < 0){
 		perror("fork: ");
 		exit(1);
 	}
         if(childPid == 0){
-		if(execlp("sort", "sort", "-k", "4","-n", argc[i], "-o", aux, (char*)NULL) == -1){
+		if(execlp("sort", "sort", "-k", "4","-n", "-k5,6", argc[i], "-o", aux, (char*)NULL) == -1){
 			perror("Child sort error: ");
 			exit(1);
 		}
 		exit(0);
 	}
     }
+
     for(i = 1; i < argv-1; i++){ wait(&status);}
     aux = malloc(10*sizeof(*aux));
     strcpy(aux, "-o");	
-    command[argv+2] = aux;
-    strcpy(command[argv+3], argc[argv-1]);
-    command[argv+4] = (char*)NULL;
+    command[argv+3] = aux;
+    command[argv+4] = malloc(sizeof(char)*100);
+    strcpy(command[argv+4], argc[argv-1]);
+    command[argv+5] = (char*)NULL;
+
     if(execvp("sort", command ) == -1){
 			perror("Parent sort error: ");
 			exit(1);
